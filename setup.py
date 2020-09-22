9e19e02f397105fe258220c49ebe1187afbf6d49
luk-pio <luke.piotrak@gmail.com>
2020-09-22 18:28:15 +0200

    Refactor to cookiecutter template. Rewrite preprocessing.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An analysis of dimensionality reduction and music information retrieval techniques for the visual representation of large audio dataset_configs',
+    author='Lukasz Piotrak',
+    license='MIT',
+)
---
+++ src/common/log.py
@@ -0,0 +1,5 @@
+import logging
+
+log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=log_fmt)
+
