7051afe2e9dc58080fbd87c9454a8eb81198bc91
luk-pio <luke.piotrak@gmail.com>
2020-11-14 11:55:41 +0100

    Formatting

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='An analysis of dimensionality reduction and music information retrieval techniques for the visual representation of large audio dataset_configs',
-    author='Lukasz Piotrak',
-    license='MIT',
+    version="0.1.0",
+    description="An analysis of dimensionality reduction and music "
+    "information retrieval techniques for the visual representation of large audio dataset_configs",
+    author="Lukasz Piotrak",
+    license="MIT",
 )
