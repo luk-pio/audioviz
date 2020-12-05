1584535bacb4a821ae4a0ee960a74cdfbe18bb07
luk-pio <luke.piotrak@gmail.com>
2020-12-05 20:36:01 +0100

    Preproc to hdf

--- src/common/audioviz_dataset.py
+++ src/common/audioviz_dataset.py
@@ -1,16 +1,17 @@
-import logging
-import os
-from typing import List
+import gc
+from collections import defaultdict
+from typing import List, Dict, Any
 
 import numpy as np
 import pandas as pd
 from sklearn.utils import Bunch
 
-from src.common.dataset_config import dataset_config_factory
-from src.common.utils import DATA_INTERIM_DIR
-
 # noinspection PyUnresolvedReferences
 import src.common.log
+from src.common.audioviz_datastore import (
+    AbstractAudiovizDataStore,
+    AudiovizDataStoreFactory,
+)
 
 
 class AudiovizDataset(Bunch):
@@ -70,36 +71,171 @@ class AudiovizDataset(Bunch):
             metadata=metadata,
         )
 
+    # @staticmethod
+    # def load(name: str, store: AbstractAudiovizDataStore = None):
+    #     config = dataset_config_factory(name)
+    #     if store is None:
+    #         file_prefix = config.get_name()
+    #         file_path = os.path.join(DATA_INTERIM_DIR, file_prefix + "_data.h5")
+    #         store = Hdf5AudiovizDataStore(file_path)
+    #     data = store[]
+    #     return AudiovizDataset(
+    #         data=data,
+    #         target=metadata["class"],
+    #         name=name,
+    #         target_names=config.get_classes(),
+    #         data_filename=file_path,
+    #         metadata_filename=metadata_path,
+    #         metadata=metadata,
+    #     )
 
-def load_audioviz_dataset(name):
-    config = dataset_config_factory(name)
-    file_prefix = config.get_name()
-    data_path = os.path.join(DATA_INTERIM_DIR, file_prefix + "_data.npy")
-    metadata_path = os.path.join(DATA_INTERIM_DIR, file_prefix + "_metadata.csv")
-    try:
-        data = np.load(data_path)
-    except IOError:
-        logging.error(
-            f"Could not read file at {data_path}. Have you run preprocessing for this dataset?"
-        )
-        raise
-    try:
-        metadata = pd.read_csv(metadata_path)
-    except IOError:
-        logging.error(
-            f"Could not read file at {metadata_path}. Have you run preprocessing for this dataset?"
-        )
-        raise
-    return AudiovizDataset(
-        data=data,
-        target=metadata["class"],
-        name=name,
-        target_names=config.get_classes(),
-        data_filename=data_path,
-        metadata_filename=metadata_path,
-        metadata=metadata,
+    # @staticmethod
+    # def dump(directory, fieldnames, files_metadata, root_dir, samples):
+    #     data_path = os.path.join(root_dir, directory + "_data.h5")
+    #     np.asarray(samples):
+    #         logging.info(f"Saved {len(samples)} samples of {directory} to {data_path}")
+    #     else:
+    #         logging.error(
+    #             f"Failed to save {len(samples)} samples of {directory} to {data_path}"
+    #         )
+    #     metadata_path = os.path.join(root_dir, directory + "_metadata.csv")
+    #     try:
+    #         write_dict_as_csv(metadata_path, files_metadata, fieldnames)
+    #         logging.info(f"Wrote metadata to file {metadata_path}")
+    #     except OSError:
+    #         logging.exception(f"Could not write metadata file to {metadata_path}.")
+
+
+def save_medley_solos_db(
+    name: str,
+    dataset: Dict[str, Any],
+    path: str,
+    metadata_keys: List[str],
+    store: AbstractAudiovizDataStore = None,
+):
+    store = (
+        AudiovizDataStoreFactory.get_instance(path, "h5") if store is None else store
     )
 
+    subsets_mapping = {"training": 0, "test": 1, "validation": 2}
+    subsets_list = ["training", "test", "validation"]
+    classes = [
+        "clarinet",
+        "distorted electric guitar",
+        "female singer",
+        "flute",
+        "piano",
+        "tenor saxophone",
+        "trumpet",
+        "violin",
+    ]
+    samplerate = next(iter(dataset.items()))[1].audio[1]
+    dataset_metadata = {
+        "subsets_list": subsets_list,
+        "classes": classes,
+        "samplerate": samplerate,
+    }
 
-def load_medley_solos_db():
-    load_audioviz_dataset("medley_solos_db")
+    samples = []
+    files_metadata = defaultdict(list)
+    sample_shape = (
+        next(iter(dataset.items()))[1].audio[0].shape
+    )  # Gets the length of the sample
+
+    shape = (len(dataset), *sample_shape)
+
+    with store:
+        store._file.create_dataset(name, shape=shape)
+    i = 0
+    chunksize = 200
+    for _, sample in dataset.items():
+        samples.append(sample.audio[0])
+        # Turn list of metadata dictionaries for each file into one dict with a list of vals
+        for key in ["instrument_id", "subset", "song_id"]:
+            value = getattr(sample, key)
+            # Convert subset to nominal
+            if key == "subset":
+                value = subsets_mapping[value]
+            files_metadata[key].append(value)
+        i += 1
+        if i % 100 == 0:
+            print(f"Processed {i} samples")
+        if i % chunksize == 0:
+            with store:
+                ds = store[name]
+                ds[i - chunksize : i] = np.asarray(samples)
+            print(f"Wrote chunk: [{i-chunksize}:{i}]")
+        del samples
+        gc.collect()
+        samples = []
+    with store:
+        ds = store[name]
+        remainder = i % chunksize
+        ds[i - remainder : i] = np.asarray(samples)
+        for k, v in files_metadata.items():
+            store._file.create_dataset(k, data=v)
+        for k, v in dataset_metadata.items():
+            ds.attrs[k] = v
+
+    del samples
+
+    store.close()
+
+
+# class AudiovizDatasetFactory:
+#     implemented = {"medley-solos-db": create_medley_solos_ds}
+#
+#     @classmethod
+#     def get_instance(cls, dataset: str):
+#         try:
+#             return cls.implemented[dataset]
+#         except KeyError as err:
+#             msg = f"The dataset {dataset} has not been implemented"
+#             logging.exception(msg)
+#             raise NotImplementedError(msg) from err
+#
+# @staticmethod
+# def load(name):
+#     config = dataset_config_factory(name)
+#     file_prefix = config.get_name()
+#     data_path = os.path.join(DATA_INTERIM_DIR, file_prefix + "_data.npy")
+#     metadata_path = os.path.join(DATA_INTERIM_DIR, file_prefix + "_metadata.csv")
+#     try:
+#         data = np.load(data_path)
+#     except IOError:
+#         logging.error(
+#             f"Could not read file at {data_path}. Have you run preprocessing for this dataset?"
+#         )
+#         raise
+#     try:
+#         metadata = pd.read_csv(metadata_path)
+#     except IOError:
+#         logging.error(
+#             f"Could not read file at {metadata_path}. Have you run preprocessing for this dataset?"
+#         )
+#         raise
+#     return AudiovizDataset(
+#         data=data,
+#         target=metadata["class"],
+#         name=name,
+#         target_names=config.get_classes(),
+#         data_filename=data_path,
+#         metadata_filename=metadata_path,
+#         metadata=metadata,
+#     )
+#
+# @staticmethod
+# def dump(directory, fieldnames, files_metadata, root_dir, samples):
+#     data_path = os.path.join(root_dir, directory + "_data.npy")
+#     if write_ndarray(data_path, np.asarray(samples)):
+#         logging.info(f"Saved {len(samples)} samples of {directory} to {data_path}")
+#     else:
+#         logging.error(
+#             f"Failed to save {len(samples)} samples of {directory} to {data_path}"
+#         )
+#     metadata_path = os.path.join(root_dir, directory + "_metadata.csv")
+#     try:
+#         write_dict_as_csv(metadata_path, files_metadata, fieldnames)
+#         logging.info(f"Wrote metadata to file {metadata_path}")
+#     except OSError:
+#         logging.exception(f"Could not write metadata file to {metadata_path}.")
--- src/common/utils.py
+++ src/common/utils.py
@@ -7,7 +7,7 @@ import numpy as np
 PROJECT_DIR = Path(__file__).resolve().parents[2]
 
 DATA_DIR = os.path.join(PROJECT_DIR, "data")
-DATA_RAW_DIR = os.path.join(DATA_DIR, "interim")
+DATA_RAW_DIR = os.path.join(DATA_DIR, "raw")
 DATA_INTERIM_DIR = os.path.join(DATA_DIR, "interim")
 DATA_PROCESSED_DIR = os.path.join(DATA_DIR, "processed")
 
